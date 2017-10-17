3e331d7e73e7e587d19f2dd4d6da16cb72c5e1ac
Gavin L. Rulon <gavin@beckshybrids.com>
2017-10-17 10:03:49 -0400

    inital push of file search program.

---
+++ fileSearch.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 09:23:31 2017
+
+@author: glrulon
+"""
+
+
+def main():
+    print_header()
+
+
+
+def print_header():
+    print('-----------------------------------------')    
+    print('           FILE SEARCH ')    
+    print('-----------------------------------------')
+    
+    
+def get_folder_from_user():
+    pass
+
+def get_search_text_from_user():
+    pass
+
+
+    
+    
+
+    
+
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+
+    
